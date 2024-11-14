202ad0a87f194039f237479ce900372e85931d12
Huong Scarlette <huong.scarlette@gmail.com>
2024-11-14 14:56:06 +0700

    Simplify creating matching keys & add area metrics
    Simplify creating matching keys:
    - Remove inserting space since it causes lots of re-modification -> match by non-space texts instead
    - No need administrative title columns
    
    Add area metrics:
    - Measure area of each ward/district (square meter) to later, calculate their population density
    
    Combine exec() functions into a single function.

--- src/data_quality/vn_boundaries.py
+++ src/data_quality/vn_boundaries.py
@@ -43,50 +43,52 @@ def reverse_bracket(word):
         word_no_bracket = word
     return word_no_bracket
 
-def only_latin_char(word):
-    word_no_diacritic = unidecode(word).lower()
-    word_no_space_specialchar = ''.join(e for e in word_no_diacritic if e.isalnum())
-    return word_no_space_specialchar
-
 def fix_admin_names(df):
     for col in ["city","district","ward"]:
         # Tackle districts like CaoLãnh(Thànhphố) or wards like AnChâu(Thịtrấn)
-        df[col] = df[col].apply(lambda x: insert_space(reverse_bracket(x)).replace("Thànhphố","Thành Phố").replace("Thịxã","Thị Xã").replace("Thịtrấn","Thị Trấn"))
+        df[col] = df[col].apply(lambda x: reverse_bracket(x).replace("Thànhphố","ThànhPhố").replace("Thịxã","ThịXã").replace("Thịtrấn","ThịTrấn"))
         # Rename mispelling districts & wards
-        df["district"] = df["district"].apply(lambda x: x.replace('Qui Nhơn','Quy Nhơn').replace('Tân Thành','Phú Mỹ'))
-        df["ward"] = df["ward"].apply(lambda x: x.replace("Cầukho","Cầu Kho").replace("Trực Phú","Ninh Cường").replace("Việt Khái","Nguyễn Việt Khái")
-                                                 .replace('Chương Dương Độ','Chương Dương').replace("Phiêng Côn","Phiêng Kôn")
-                                                 .replace('Trungtâmhuấnluyện','TTHL').replace("Cư Yang","Cư Jang"))
-        # df["ward"][df.ward_id=="VNM.39.1.15_1"] = "Thanh Phú"
-        df.loc[df[df.ward_id=="VNM.39.1.15_1"].index, "ward"] = "Thanh Phú"
+        df["district"] = df["district"].apply(lambda x: x.replace("QuiNhơn","QuyNhơn").replace("TânThành","PhúMỹ"))
+        df["ward"] = df["ward"].replace({"ViệtKhái":"NguyễnViệtKhái","TrựcPhú":"NinhCường","ChươngDươngĐộ":"ChươngDương",
+                                       "PhiêngCôn":"PhiêngKôn","Trungtâmhuấnluyện":"TTHL","CưYang":"CưJang","Cầukho":"CầuKho"})
+        df.loc[df[df.ward_id=="VNM.39.1.15_1"].index, "ward"] = "ThanhPhú"
     return df
 
 def adminkeys_to_match(df):
-    # Create matching keys between datasets by transforming administrative names
-    df["dist_title"] = df["district"]
-    df["dist_en"] = df["district"]
-    df["ward_en"] = df["ward"]
+    # Original administrative names - To show on dashboard
+    for col in ["city","district","ward"]:
+        df[col+"_org"] = df[col].apply(lambda x: insert_space(x))
+    
+    # Split administrative names into names and titles (e.g. Thị Xã Buôn Hồ)
     for i in range(len(df)):
-        # Split administrative names into names and titles (e.g. Thị Xã Buôn Hồ)
-        for d in ["Thành Phố","Quận","Thị Xã"]:
-            df.loc[i, "dist_en"] = df.dist_en[i].replace(d,"")
-            df.loc[i, "dist_title"] = df.dist_title[i].replace(df.dist_en[i],"")
-        for w in ['Huyện','Phường','Thị Trấn','Xã']:
-            df.loc[i, "ward_en"] = df.ward_en[i].replace(w,"")
-    # Remove all diacritics, spaces and special characters
-    for col in ["ward_title","ward_en","dist_title","dist_en"]:
-        df[col] = df[col].apply(lambda x: only_latin_char(x))
+        for c in ["ThànhPhố","Tỉnh"]:
+            df.loc[i,"city"] = df.city[i].replace(c,"")
+        for d in ["ThànhPhố","Quận","ThịXã","Huyện"]:
+            df.loc[i,"district"] = df.district[i].replace(d,"")
+        for w in ["Huyện","Phường","Thị Trấn","Xã"]:
+            df.loc[i,"ward"] = df.ward[i].replace(w,"")
+    
     # Some district just has title&number, thus need to insert the title "Quận" back to these districts
-    df["dist_en"] = df["dist_en"].apply(lambda x: "quan"+str(int(x)) if x.isnumeric() else only_latin_char(x))
+    df["district"] = df["district"].apply(lambda x: "Quận"+str(int(x)) if x.isnumeric() else x)
     # Some ward just has title&number, thus need to insert the title "Phường" back to these wards
-    df["ward_en"] = df["ward_en"].apply(lambda x: "phuong"+str(int(x)) if x.isnumeric() else only_latin_char(x))
-    df.loc[df[df.dist_id=="VNM.24.7_1"].index, "ward_en"] = df[df.dist_id=="VNM.24.7_1"].ward_en.apply(lambda x: "phuong"+x if len(x) < 4 else x) ##phườngIV
+    df["ward"] = df["ward"].apply(lambda x: "Phường"+str(int(x)) if x.isnumeric() else x)
+    df.loc[df[df.dist_id=="VNM.24.7_1"].index, "ward"] = df[df.dist_id=="VNM.24.7_1"].ward.apply(lambda x: "Phường"+x if len(x) < 4 else x) ##phườngIV
+    
+    # After transformation, some districts/wards are duplicated, thus need to revert them back to their original names
+    dup_dist = df.groupby(["city","district"])["dist_id"].transform('nunique').gt(1)
+    df.loc[dup_dist,"district"] = df.loc[dup_dist,"district_org"].apply(lambda x: x.replace(" ",""))
+    dup_ward = df.groupby(["city","district","ward"])["ward_id"].transform('nunique').gt(1)
+    df.loc[dup_ward,"ward"] = df.loc[dup_ward,"ward_org"].apply(lambda x: x.replace(" ",""))
+    
+    # Buffer matching keys by removing all diacritics
+    df["dist_en"] = df["district"].apply(lambda x: unidecode(x).lower())
+    df["ward_en"] = df["ward"].apply(lambda x: unidecode(x).lower())
     return df
 
 def read_json(filepath, cols, newcols):
     df = gpd.read_file(filepath)
     df = df[df.columns[cols]]
-    df = df.set_axis(newcols, axis=1)
+    df.columns = newcols
     df = fix_admin_names(df)
     # To match with Population
     df = adminkeys_to_match(df)
@@ -97,46 +99,56 @@ def read_shp(filepath, cols, newcols):
     df = gpd.GeoDataFrame(df, crs=32648)
     df = df.to_crs(4326)
     df = df[df.columns[cols]]
-    df = df.set_axis(newcols, axis=1)
+    df.columns = newcols
     # To match with GADM
-    df["dist_en"] = df["dist_en"].apply(lambda x: only_latin_char(x.replace("District","Quan")))
-    df["ward_en"] = df["ward_en"].apply(lambda x: only_latin_char(x.replace("Ward","Phuong")))
+    df["dist_en"] = df["dist_en"].apply(lambda x: x.replace("District","Quan").lower().replace(" ",""))
+    df["ward_en"] = df["ward_en"].apply(lambda x: x.replace("Ward","Phuong").lower().replace(" ",""))
+    return df
+
+def measure_area(df):
+    # convert CRS to equal-area projection -> the length unit is now `meter`
+    df = df.df(epsg=6933)
+    df["area_sqm"] = df.area.values #/10e6 for sqKm
+    df = df.to_crs(epsg=4326)
     return df
 
 def get_representative_loc(df, admin_level):
     df_admin = df[[admin_level,"geometry"]].dissolve(by=admin_level, as_index=False)
-    df_admin["loc"] = df_admin.representative_point()
-    df = pd.merge(df, df_admin[[admin_level,"loc"]], how="inner", on=[admin_level])
+    df_admin["city_centroid"] = df_admin.representative_point()
+    df = pd.merge(df, df_admin[[admin_level,"city_centroid"]], how="inner", on=[admin_level])
     return df
 
-def exec_ward():
+def exec(admin_level):
     # Source: https://gadm.org/download_country.html (level 3 = Ward)
     # Note: GADM administrative names don't have spaces
     gadm_boundaries = read_json(filepath=r"data\gadm41_VNM_3.json",
-                                cols=[2,4,7,6,9,0,12,16],
-                                newcols=["country","city","district","dist_id","ward","ward_id","ward_title","geometry"],
-                                cols_to_fix=["city","district","ward","ward_title"]
+                                cols=[2,4,7,6,9,0,16],
+                                newcols=["country","city","district","dist_id","ward","ward_id","geometry"],
                                 )
 
     # Source: https://github.com/CityScope/CSL_HCMC/tree/main/Data/GIS/Population/population_HCMC/population_shapefile
     cityscope_boundaries = read_shp(filepath=r"data\CityScope_HCMC\Population_Ward_Level.shp",
                                     cols=[0,1,2,11],
-                                    newcols=["ward_en","district_en","cityscope_id","geometry"]
+                                    newcols=["ward_en","dist_en","cityscope_id","geometry"]
                                     )
 
     ## Merge 2 boundaries
-    hcmc_boundaries = gadm_boundaries[gadm_boundaries.city=="Hồ Chí Minh"].drop(["geometry"], axis=1)
-    hcmc_boundaries = pd.merge(hcmc_boundaries, cityscope_boundaries, how="inner", on=["district_en", "ward_en"])
+    hcmc_boundaries = gadm_boundaries[gadm_boundaries.city=="HồChíMinh"].drop(["geometry"], axis=1)
+    hcmc_boundaries = pd.merge(hcmc_boundaries, cityscope_boundaries, how="inner", on=["dist_en","ward_en"])
     hcmc_boundaries = gpd.GeoDataFrame(hcmc_boundaries)
 
     # Append modified HCMC boundaries to GADM
-    ward_boundaries = pd.concat([gadm_boundaries[gadm_boundaries.city!="Hồ Chí Minh"], hcmc_boundaries])
-    ward_boundaries = gpd.GeoDataFrame(ward_boundaries.drop(["cityscope_id","district_en", "ward_en","ward_title_en"], axis=1))
-    ward_boundaries = get_representative_loc(ward_boundaries, "city")
-    return ward_boundaries
+    ward_boundaries = pd.concat([gadm_boundaries[gadm_boundaries.city!="HồChíMinh"], hcmc_boundaries])
+    ward_boundaries = gpd.GeoDataFrame(ward_boundaries.drop(["cityscope_id"], axis=1))
 
-def exec_dist():
-    ward_boundaries = exec_ward()
-    dist_boundaries = ward_boundaries.drop(["ward","ward_id","ward_title","ward_en"], axis=1)
-    dist_boundaries = dist_boundaries.dissolve(by=["country","city","district","dist_title","dist_en","dist_id"], as_index=False)
-    return dist_boundaries
+    # Measure area size to later calculate population density
+    ward_boundaries = measure_area(ward_boundaries)
+    # Get city central point to navigate map easily
+    ward_boundaries = get_representative_loc(ward_boundaries, "city")
+    
+    dist_boundaries = ward_boundaries[[col for col in ward_boundaries.columns if "ward" not in col and "area" not in col]]
+    dist_boundaries = dist_boundaries.dissolve(by=["country","city","district","dist_id","city_org","district_org","dist_en"], as_index=False)
+    # Measure area size to later calculate population density
+    dist_boundaries = dist_boundaries(ward_boundaries)
+
+    return ward_boundaries if admin_level=="ward" else dist_boundaries
